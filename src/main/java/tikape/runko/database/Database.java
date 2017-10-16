359e85ccf1339437fe4d964fecab1e1b7dd0e719
Savolainen95 <mikko.i.savolainen@gmail.com>
2017-10-16 15:01:55 +0300

    hiio hei

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -40,14 +40,27 @@ public class Database {
         
         
         lista.add("CREATE TABLE Annos (id integer PRIMARY KEY, nimi varchar(255));");
-                
-        lista.add("CREATE TABLE RaakaAine (id integer PRIMARY KEY, nimi varchar(255));");
+        lista.add("INSERT INTO Annos (nimi) VALUES ('voileipä');"); //1
+        lista.add("INSERT INTO Annos (nimi) VALUES ('hernerokka');"); //2
+        lista.add("INSERT INTO Annos (nimi) VALUES ('vesikeitto');"); //3
+        lista.add("INSERT INTO Annos (nimi) VALUES ('kaurapuuro');"); //4
         
-        lista.add("CREATE TABLE AnnosRaakaAine FOREIGN KEY (raaka_aine_id) REFERENCES RaakaAine(id) ,"
-                + " FOREIGN KEY (annos_id) REFERENCES Annos(id),"
+        lista.add("CREATE TABLE RaakaAine (id integer PRIMARY KEY, nimi varchar(255));");
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('vesi');"); //1
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('voi');");//2
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('kaurahiutaleita');");//3
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('leipä');");//4
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('hernekeitto');");//5
+        
+        lista.add("CREATE TABLE AnnosRaakaAine  ,"
                 + " jarjestys integer,"
-                + " maara integer,"
-                + " ohje varchar(3000);");
+                + " maara varchar(100),"
+                + " ohje varchar(3000),"
+                + " FOREIGN KEY (raaka_aine_id) REFERENCES RaakaAine(id),"
+                + " FOREIGN KEY (annos_id) REFERENCES Annos(id));");
+
+        lista.add("INSERT INTO AnnosRaakaAine (raaka_aine_id, annos_id, jarjestys, maara, ohje)"
+                + "VALUES (1, 3, 1, 'sopivasti', 'keitä vesi lämpimäksi')");
         
         //lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Homeros');");
 
