52407a1f33f3a5d812ca706130c4068433ce962e
Savolainen95 <mikko.i.savolainen@gmail.com>
2017-10-16 15:47:52 +0300

    lisätty

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -53,9 +53,11 @@ public class Database {
         lista.add("INSERT INTO RaakaAine (nimi) VALUES ('hernekeitto');");//5
         
         lista.add("CREATE TABLE AnnosRaakaAine "
-                + "(jarjestys integer,"
+                + "(raaka_aine_id integer,"
+                + " annos_id integer,"
+                + " jarjestys integer,"
                 + " maara varchar(100),"
-                + " ohje varchar(3000),"
+                + " ohje varchar(300),"
                 + " FOREIGN KEY (raaka_aine_id) REFERENCES RaakaAine(id),"
                 + " FOREIGN KEY (annos_id) REFERENCES Annos(id));");
 
