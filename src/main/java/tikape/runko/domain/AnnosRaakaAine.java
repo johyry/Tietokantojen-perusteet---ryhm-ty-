53b5db5cabc46eb33e5bdacdfb577d0d916bc0ce
Savolainen95 <mikko.i.savolainen@gmail.com>
2017-10-16 13:23:28 +0300

    AnnosRaakaAine lisatty settereilla ja gettereilla

---
+++ src/main/java/tikape/runko/domain/AnnosRaakaAine.java
@@ -0,0 +1,61 @@
+
+package tikape.runko.domain;
+
+public class AnnosRaakaAine {
+    
+    private int jarjestys;
+    private int maara;
+    private String ohje;
+    private Integer raaka_aine_id;
+    private Integer annos_id;
+    
+    
+    public AnnosRaakaAine() {
+        
+    }
+    public AnnosRaakaAine(Integer jarjestys, Integer maara, String ohje, Integer raaka_aine_id, Integer annos_id) {
+        this.jarjestys = jarjestys;
+        this.maara = maara;
+        this.ohje = ohje;
+        this.raaka_aine_id = raaka_aine_id;
+        this.annos_id = annos_id;
+    }
+    
+
+    public Integer getRaaka_aine_id() {
+        return raaka_aine_id;
+    }
+
+    public Integer getAnnos_id() {
+        return annos_id;
+    }
+    public Integer getMaara() {
+        return maara;
+    }
+
+    public String getOhje() {
+        return ohje;
+    }
+
+    public void setRaaka_aine_id(Integer raaka_aine_id) {
+        this.raaka_aine_id = raaka_aine_id;
+    }
+
+    public void setAnnos_id(Integer annos_id) {
+        this.annos_id = annos_id;
+    }
+
+    public void setMaara(Integer maara) {
+        this.maara = maara;
+    }
+
+    public void setOhje(String ohje) {
+        this.ohje = ohje;
+    }
+
+    
+    
+    
+    
+    
+}
